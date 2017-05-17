8716d8d7c8f839abea769a51fa0341541e8983bb
Francesco Peverelli <francescopeverelli@hotmail.it>
2017-05-17 14:49:19 +0200

    test commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,56 @@
+/**
+ * Main class for the project.
+ * @author Francesco Peverelli, Marco Siracusa
+ */
+ 
 #include <iostream>
 #include <utility>
 #include <memory>
 
-using namespace std;
-
 #include "llvm/IR/Module.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm-c/Core.h"
-
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/AsmParser/Parser.h"
 
-int setResourceDir(const string &resourceDir){
+using namespace std;
+using namespace llvm;
+
+//Directory used to access .ll files to process
+const string RESOURCES_DIR; 
+//Directory used to store the processed files
+const string OUTPUT_DIR;
+
+int extractModuleFromFile(const string fileName){
         
     }
 
-using namespace llvm;
-
 int main(int argc, char**argv) {
+    
+    //set directory to look for .ll files
+    RESOURCES_DIR = argv[2];
+    //set file to process. Must be a .ll file containing a llvm module
+    const string fileName = argv[1];
+    
+    
+    extractModuleFromFile(fileName);
 
-  static LLVMContext context;
-  Module* module = new Module("top", context);
+    static LLVMContext context;
+    Module* module = new Module("top", context);
 
-  IRBuilder<> builder(context);
+    IRBuilder<> builder(context);
 
-  module->dump();
+    module->dump();
 
-  StringRef fileName = "/home/user/Documents/workspace/prova/resources/loop_1.ll";
-  SMDiagnostic error;
+    StringRef fileName = "/home/user/Documents/workspace/prova/resources/loop_1.ll";
+    SMDiagnostic error;
 
-  auto parsedModuleCont = parseAssemblyFile(fileName,error,context);
+    auto parsedModuleCont = parseAssemblyFile(fileName,error,context);
 
-  Module* parsedModule = parsedModuleCont.get();
+    Module* parsedModule = parsedModuleCont.get();
 
-  ((Module*)parsedModule)->dump();
+    ((Module*)parsedModule)->dump();
 
 
   return 0;
