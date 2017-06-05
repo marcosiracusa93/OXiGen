e9e728becd4ed26e22c3e0c878afcb45e2cdedc4
Marco Siracusa <sira@Marcos-MacBook-Pro.local>
2017-06-05 17:48:24 +0200

    TestPass draft, NOT COMPILING

--- src/main.cpp
+++ src/main.cpp
@@ -13,43 +13,112 @@
 #include "llvm-c/Core.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/AsmParser/Parser.h"
+#include "llvm/IR/Dominators.h"
 
-using namespace std;
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Analysis/BasicAliasAnalysis.h"
+#include "llvm/Analysis/AliasAnalysis.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/Analysis/ScalarEvolutionAliasAnalysis.h"
+
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+
+
 using namespace llvm;
 
-//Directory used to access .ll files to process
-string RESOURCES_DIR; 
-//Directory used to store the processed files
-string OUTPUT_DIR;
 
+
+/*
+namespace{
+
+		
+	struct TestPass : public FunctionPass {
+		static char ID;
+		TestPass() : FunctionPass(ID) {}
+
+	  	bool runOnFunction(Function &F) override {
+	    	errs() << "Hello: ";
+	    	errs().write_escaped(F.getName()) << '\n';
+	    	return false;
+	  	}
+	}; // end of struct TestPass	
+
+	char TestPass::ID = 0;
+	static RegisterPass<TestPass> X("testPass", "Pass used for testing",
+    							 	false,
+                             		true);
+} // end anonymous namespace
+
+FunctionPass* createTestWrapperPass() {
+	return new TestPass();
+}
+*/
+
+
+
+
+
+//Directory used to access .ll files to process
+std::string RESOURCES_DIR; 
+//Directory used to store the processed files
+std::string OUTPUT_DIR;
+
+/**
+ * @brief Entry point of the program. Two arguments must be passed. 
+ * @param argc 
+ * @param argv [1]: name of a .ll file, [2]: directory where [1] is located
+ * @return 0 on success, -1 on failure
+ */
 int main(int argc, char**argv) {
+	    
+    static LLVMContext context;         	// LLVMContext variable
+    SMDiagnostic error;                 	// error message
+    Module* module;                     	// module to process
     
-    static LLVMContext context;         //LLVMContext variable
-    SMDiagnostic error;                 //error message
-    Module* module;                     //module to process
-    StringRef filePathRef;              //StringRef for the file to process
+    StringRef filePathRef;              	// StringRef for the file to process
+    std::string fileName;               	// file to process, must be a .ll file containing a llvm module
+    std::string filePath;               	// full path for the file to process
     
-    ::RESOURCES_DIR = argv[2];          //set directory to look for .ll files
-    const string fileName = argv[1];    //set file to process. Must be a .ll file containing a llvm module
+    ::RESOURCES_DIR = argv[2];          	// set directory to look for .ll files
+    fileName = argv[1];                 	// set name of the file to process
     
-    const string filePath = ::RESOURCES_DIR + fileName;
-    filePathRef = filePath;
+    filePath = ::RESOURCES_DIR + fileName; 	// set full path
+    filePathRef = filePath;                	// set a StringRef for the full path
     
     try{
         //try to open the file to process
-        fstream file;
+        std::fstream file;
         file.open(filePath);
         file.close();
         
-    }catch(const exception &e){
-        cout << "Failed to open " + filePath;
+    }catch(const std::exception &e){
+        std::cout << "Failed to open " + filePath;
         return -1;
     }
     
-    module = (Module*)parseAssemblyFile(filePathRef,error,context).get();
-
-    module->dump();
+    //parse a llvm::Module from the .ll file 
+    auto modPtr = parseAssemblyFile(filePathRef,error,context);
     
+    if(modPtr == NULL){
+        std::cout << "Null pointer returned from parsing, abort" << std::endl;
+        return -1;
+    }
+    
+    module = (Module*)modPtr.get();
+
+    legacy::FunctionPassManager* functionPassManager = new legacy::FunctionPassManager(module);
+	
+	functionPassManager->add(createBasicAAWrapperPass());			// -basicaa
+	functionPassManager->add(createAAResultsWrapperPass());			// -aa
+	functionPassManager->add(createPromoteMemoryToRegisterPass());	// -mem2reg
+	functionPassManager->add(createSCEVAAWrapperPass());			// -scalar-evolution
+//	functionPassManager->add(createTestWrapperPass());
+	FunctionPass* fp = createBasicAAWrapperPass();
+	functionPassManager->run(*module->getFunction(StringRef("main")));
+	
     return 0;
 }
